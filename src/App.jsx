a1131b9a39fd31d19e8768a40dab5bd5ebf8d06f
habibur-pro <va.habibur@gmail.com>
2023-07-02 13:25:15 +0600

    added home

--- src/App.jsx
+++ src/App.jsx
@@ -4,11 +4,11 @@ import Sidebar from "./components/Sidebar/Sidebar";
 
 const App = () => {
   return (
-    <div className="max-w-[1500px] md:grid grid-cols-6">
-      <div className="col-span-1 h-screen">
+    <div className="max-w-[1500px] md:grid grid-cols-6 px-3 md:px-0">
+      <div className="md:col-span-1 h-screen">
         <Sidebar></Sidebar>
       </div>
-      <div className="col-span-3 md: pl-16 pt-10">
+      <div className="md:col-span-4  md:pl-16 ">
         <Home />
       </div>
     </div>
--- src/components/Home/Home.jsx
+++ src/components/Home/Home.jsx
@@ -1,12 +1,18 @@
-
+import photo from '../../assets/photo.jpg'
 
 const Home = () => {
     return (
-        <div>
-            <h3 className="text-2xl font-bold text-white">Hello I am</h3>
-            <h1 className="text-5xl font-bold text-white my-7">Habibur Rahman</h1>
-            <h4 className="font-bold text-xl">I am Professional Web Developer</h4>
-            <p className="mt-5">Lorem ipsum dolor sit amet consectetur adipisicing elit. Ex magni, fugiat tempora dignissimos iure minima assumenda animi reprehenderit repellendus commodi!</p>
+        <div id='home' className='md:flex  justify-between md:gap-10 min-h-screen items-center  w-full'>
+            <div className='md:w-1/2'>
+                <h3 className="text-2xl font-bold text-white">Hello I am</h3>
+                <h1 className="text-5xl font-bold text-white my-7">Habibur Rahman</h1>
+                <h4 className="font-bold text-xl">I am Professional Web Developer</h4>
+                <p className="mt-5 text-lg font-normal">Lorem ipsum dolor sit amet consectetur adipisicing elit. Ex magni, fugiat tempora dignissimos iure minima assumenda animi reprehenderit repellendus commodi!</p>
+                <button className='btn btn-primary rounded-sm text-white mt-8'>Download Resume</button>
+            </div>
+            <div className='md:w-1/2'>
+                <img className=' rounded-full' src={photo} alt="" />
+            </div>
         </div>
     );
 };
