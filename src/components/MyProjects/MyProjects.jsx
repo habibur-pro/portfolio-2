34a0fc38394dd05d8978d06bbfdb6332319bb559
habibur-pro <va.habibur@gmail.com>
2023-07-04 15:22:02 +0600

    implement live links

--- src/components/MyProjects/MyProjects.jsx
+++ src/components/MyProjects/MyProjects.jsx
@@ -7,18 +7,18 @@ import { Link } from "react-router-dom";
 
 const MyProjects = () => {
     return (
-        <div className="h-screen" id="projects">
+        <div className="py-20" id="projects">
             <h1 className="text-3xl text-center">My Projects</h1>
             <Underline />
 
-            <div className="grid grid-cols-1 md:grid-cols-3 gap-10">
+            <div className="grid grid-cols-1 md:grid-cols-3 gap-8">
                 {/* project 1  */}
-                <div>
-                    <div className=" h-[300px] bg-cover ease-linear border bg-top hover:bg-bottom duration-[1.5s] rounded-t-lg" style={{ backgroundImage: `url(${artZone})` }}>
+                <div className="group relative">
+                    <div className=" h-[300px] bg-cover ease-linear border bg-top group-hover:bg-bottom duration-[1.5s] rounded-t-lg" style={{ backgroundImage: `url(${artZone})` }}>
                     </div>
-                    <div className="p-3 bg-secondary">
+                    <div className="p-3 bg-secondary ">
                         <h3 className="text-primary text-xl font-bold">ArtZone</h3>
-                        <p className="text-md mb-3">A full stack art learning website. It has secure login system, payment system, role based dashboard etc. </p>
+                        <p className="text-sm mb-3 ">A full stack art learning website. Students can buy and teachers cant add,update,delete class. It has secure login system, payment system, role based dashboard etc. </p>
                         <div className="flex flex-wrap gap-2">
                             <p className="bg-primary text-xs px-3 py-1 rounded-full text-white ">Html</p>
                             <p className="bg-primary text-xs px-3 py-1 rounded-full text-white ">Css</p>
@@ -29,9 +29,12 @@ const MyProjects = () => {
                             <p className="bg-primary text-xs px-3 py-1 rounded-full text-white ">Express Js</p>
                             <p className="bg-primary text-xs px-3 py-1 rounded-full text-white ">MongoDB</p>
                             <p className="bg-primary text-xs px-3 py-1 rounded-full text-white ">Firebase</p>
+                            <p className="bg-primary text-xs px-3 py-1 rounded-full text-white ">Stripe Payment</p>
+                            <p className="bg-primary text-xs px-3 py-1 rounded-full text-white ">Jwt</p>
+                            <p className="bg-primary text-xs px-3 py-1 rounded-full text-white ">Axios</p>
                         </div>
                         <hr className="my-2" />
-                        <div className="my-3 flex justify-between">
+                        <div className="my-3 flex justify-between ">
                             <button className="btn btn-accent  text-white btn-sm">
                                 < Link to='https://artzone-be3a9.web.app/' target="_blank">Live site</Link>
                             </button>
@@ -47,12 +50,12 @@ const MyProjects = () => {
 
                 </div>
                 {/* project 2  */}
-                <div>
-                    <div className=" h-[300px] bg-cover ease-linear border bg-top hover:bg-bottom duration-[1.5s] rounded-t-lg" style={{ backgroundImage: `url(${tinyCar})` }}>
+                <div className="group relative">
+                    <div className=" h-[300px] bg-cover ease-linear border bg-top group-hover:bg-bottom duration-[1.5s] rounded-t-lg" style={{ backgroundImage: `url(${tinyCar})` }}>
                     </div>
                     <div className="p-3 bg-secondary">
                         <h3 className="text-primary text-xl font-bold">TinyCar</h3>
-                        <p className="text-md mb-3">A full stack art learning website. It has secure login system, payment system, role based dashboard etc. </p>
+                        <p className="text-md mb-3 text-sm">A toy marketplace. User can select Add, update, and delete his own toys. Implemented secure login, search and sorting system. Shows toys by category. </p>
                         <div className="flex flex-wrap gap-2">
                             <p className="bg-primary text-xs px-3 py-1 rounded-full text-white ">Html</p>
                             <p className="bg-primary text-xs px-3 py-1 rounded-full text-white ">Css</p>
@@ -67,13 +70,13 @@ const MyProjects = () => {
                         <hr className="my-2" />
                         <div className="my-3 flex justify-between">
                             <button className="btn btn-accent  text-white btn-sm">
-                                < Link to='https://artzone-be3a9.web.app/' target="_blank">Live site</Link>
+                                < Link to='https://tiny-car-assignment-11.web.app/' target="_blank">Live site</Link>
                             </button>
                             <button className="btn btn-accent text-white btn-sm">
-                                < Link to='https://github.com/habibur-pro/artZone-client' target="_blank">Client</Link>
+                                < Link to='https://github.com/habibur-pro/tiny-car-client' target="_blank">Client</Link>
                             </button>
                             <button className="btn btn-accent text-white btn-sm">
-                                < Link to='https://github.com/habibur-pro/artZone-server' target="_blank">Server</Link>
+                                < Link to='https://github.com/habibur-pro/tiny-car-server' target="_blank">Server</Link>
                             </button>
 
                         </div>
@@ -82,12 +85,50 @@ const MyProjects = () => {
                 </div>
 
                 {/* project 3  */}
-                <div>
-                    <div className=" h-[300px] bg-cover ease-linear border bg-top hover:bg-bottom duration-[1.5s] rounded-t-lg" style={{ backgroundImage: `url(${SuperChefs})` }}>
+                <div className="group relative">
+                    <div className=" h-[300px] bg-cover ease-linear border bg-top group-hover:bg-bottom duration-[1.5s] rounded-t-lg" style={{ backgroundImage: `url(${SuperChefs})` }}>
                     </div>
-                    <div className="p-3 bg-secondary">
+                    <div className="p-3 bg-secondary ">
                         <h3 className="text-primary text-xl font-bold">Super Chefs</h3>
-                        <p className="text-md mb-3">A full stack art learning website. It has secure login system, payment system, role based dashboard etc. </p>
+                        <p className="text-md mb-3 text-sm">A recipe sharing website. This website responsive for all devices. A user can see recipes and select chosen recipes. selected recipe saved in local storage.  </p>
+                        <div className="flex flex-wrap gap-2">
+                            <p className="bg-primary text-xs px-3 py-1 rounded-full text-white ">Html</p>
+                            <p className="bg-primary text-xs px-3 py-1 rounded-full text-white ">Css</p>
+                            <p className="bg-primary text-xs px-3 py-1 rounded-full text-white ">Javascript</p>
+                            <p className="bg-primary text-xs px-3 py-1 rounded-full text-white ">TailwindCss</p>
+                            <p className="bg-primary text-xs px-3 py-1 rounded-full text-white ">Lazy Loader</p>
+                            <p className="bg-primary text-xs px-3 py-1 rounded-full text-white ">Swiper</p>
+                            <p className="bg-primary text-xs px-3 py-1 rounded-full text-white ">React</p>
+                            <p className="bg-primary text-xs px-3 py-1 rounded-full text-white ">Node Js</p>
+                            <p className="bg-primary text-xs px-3 py-1 rounded-full text-white ">Express Js</p>
+                            <p className="bg-primary text-xs px-3 py-1 rounded-full text-white ">Firebase</p>
+                        </div>
+                        <hr className="my-2" />
+                        <div className="my-3 flex justify-between">
+                            <button className="btn btn-accent  text-white btn-sm">
+                                < Link to='https://simple-firebase-recap-3188a.web.app/' target="_blank">Live site</Link>
+                            </button>
+                            <button className="btn btn-accent text-white btn-sm">
+                                < Link to='https://github.com/habibur-pro/super-chef-s-client' target="_blank">Client</Link>
+                            </button>
+                            <button className="btn btn-accent text-white btn-sm">
+                                < Link to='https://github.com/habibur-pro/super-chef-s-server' target="_blank">Server</Link>
+                            </button>
+
+                        </div>
+                    </div>
+
+                </div>
+                {/* project 4  */}
+                <div className="group relative">
+                    <div className=" h-[300px] bg-no-repeat ease-linear border bg-top group-hover:bg-bottom duration-[1.5s] rounded-t-lg" style={{
+                        backgroundImage: `url(${adminPanel})`,
+                        backgroundSize: '100% 100%'
+                    }}>
+                    </div>
+                    <div className="p-3 bg-secondary ">
+                        <h3 className="text-primary text-xl font-bold">Dashboard</h3>
+                        <p className="text-md mb-3 text-sm">Dashboard menu changed by user role. Secure from unAuthorize user. user, teacher or admin can not access to other user data.<br /> <span className="text-warning">Admin Email: admin@gmail.com Password:#@Admin</span></p>
                         <div className="flex flex-wrap gap-2">
                             <p className="bg-primary text-xs px-3 py-1 rounded-full text-white ">Html</p>
                             <p className="bg-primary text-xs px-3 py-1 rounded-full text-white ">Css</p>
@@ -98,11 +139,14 @@ const MyProjects = () => {
                             <p className="bg-primary text-xs px-3 py-1 rounded-full text-white ">Express Js</p>
                             <p className="bg-primary text-xs px-3 py-1 rounded-full text-white ">MongoDB</p>
                             <p className="bg-primary text-xs px-3 py-1 rounded-full text-white ">Firebase</p>
+                            <p className="bg-primary text-xs px-3 py-1 rounded-full text-white ">Stripe Payment</p>
+                            <p className="bg-primary text-xs px-3 py-1 rounded-full text-white ">Axios</p>
+                            <p className="bg-primary text-xs px-3 py-1 rounded-full text-white ">JWT</p>
                         </div>
                         <hr className="my-2" />
                         <div className="my-3 flex justify-between">
                             <button className="btn btn-accent  text-white btn-sm">
-                                < Link to='https://artzone-be3a9.web.app/' target="_blank">Live site</Link>
+                                < Link to='https://artzone-be3a9.web.app/dashboard/manage_classes' target="_blank">Live site</Link>
                             </button>
                             <button className="btn btn-accent text-white btn-sm">
                                 < Link to='https://github.com/habibur-pro/artZone-client' target="_blank">Client</Link>
